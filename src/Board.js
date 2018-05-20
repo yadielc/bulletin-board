24f4440d258b91f29ed682928899e64531251dec
Yadiel <ycabr035@fiu.edu>
2018-05-19 22:33:37 -0400

    added style enhancements to notes

--- src/Board.js
+++ src/Board.js
@@ -21,7 +21,7 @@ class Board extends Component {
     componentWillMount(){
         var self = this
         if(this.props.count){
-          fetch('https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}')
+          fetch(`https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`)
              .then(response => response.json())
              .then(json => json[0]
                .split('. ')
