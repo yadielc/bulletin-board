1a1ff42c1472efc4c8edcc6c639bd888ac17390f
Yadiel <ycabr035@fiu.edu>
2018-05-19 20:38:11 -0400

    testing

--- src/Note.js
+++ src/Note.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react'
 import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 
 class Note extends Component {
 	constructor(props) {
 		super(props)
+    this.state = {
+        editing: false
+
+    }
 		this.edit = this.edit.bind(this)
 		this.remove = this.remove.bind(this)
+    this.save= this.save.bind(this)
+    this.renderForm = this.renderForm.bind(this)
+    this.renderDisplay = this.renderDisplay.bind(this)
 	}
+
+  // Edit note function
+  // Testing
 	edit() {
-		alert('editing note')
+	    this.setState({
+        editing: true
+
+      })
 	}
+
+  // Remove note function
 	remove() {
 		alert('removing note')
 	}
-	render() {
+
+  // Save note function
+  save () {
+    alert('saving note')
+  }
+
+  renderForm() {
+    return(
+       <div className="note">
+       <form>
+        <textarea/>
+        <button onClick={this.save}><FaFloppyO/></button>
+       </form>
+       </div>
+    )
+
+  }
+
+	renderDisplay() {
 		return (
 			<div className="note">
 				<p>Learn React</p>
@@ -25,6 +59,15 @@ class Note extends Component {
 			</div>
 		)
 	}
+
+  render(){
+     if(this.state.editing){
+       return this.renderForm()
+     }
+     else {
+          return this.renderDisplay()
+     }
+  }
 }
 
 export default Note
