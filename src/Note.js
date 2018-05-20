cc38a6d3e4bad2352c66dea3afd98cb4226099b5
Yadiel <ycabr035@fiu.edu>
2018-05-19 20:29:16 -0400

    added functionalities to Note.js

--- src/Note.js
+++ src/Note.js
@@ -1,53 +1,20 @@
 import React, { Component } from 'react'
 import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
-import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 
 class Note extends Component {
 	constructor(props) {
 		super(props)
-    this.state = {
-        editing: false
-
-    }
 		this.edit = this.edit.bind(this)
 		this.remove = this.remove.bind(this)
-    this.save= this.save.bind(this)
-    this.renderForm = this.renderForm.bind(this)
-    this.renderDisplay = this.renderDisplay.bind(this)
 	}
-
-  // Edit note function
 	edit() {
-	    this.setState({
-        editing: true
-
-      })
+		alert('editing note')
 	}
-
-  // Remove note function
 	remove() {
 		alert('removing note')
 	}
-
-  // Save note function
-  save () {
-    alert('saving note')
-  }
-
-  renderForm() {
-    return(
-       <div className="note">
-       <form>
-        <textarea/>
-        <button onClick={this.save}><FaFloppyO/></button>
-       </form>
-       </div>
-    )
-
-  }
-
-	renderDisplay() {
+	render() {
 		return (
 			<div className="note">
 				<p>Learn React</p>
@@ -58,15 +25,6 @@ class Note extends Component {
 			</div>
 		)
 	}
-
-  render(){
-     if(this.state.editing){
-       return this.renderForm()
-     }
-     else {
-          return this.renderDisplay()
-     }
-  }
 }
 
 export default Note
